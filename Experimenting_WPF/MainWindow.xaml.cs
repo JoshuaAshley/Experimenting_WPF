b3add69f933dcdc89f8b9382bd204cc6e5cd88ea
JoshuaAshley <st10060590@vcconnect.edu.za>
2023-04-24 11:37:59 +0200

    00-Initial Commit

---
+++ Experimenting_WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Experimenting_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            // Minimize the window
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
